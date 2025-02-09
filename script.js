6bed3cdda6b700ce62bd5001d4f8ead7e946202f
GeauxEngrave <geauxengrave@gmail.com>
2025-02-09 16:24:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function updateText() {
     let userInput = document.getElementById('textInput').value;
-    
-    // Get all elements with the 'font-preview' class
     let previews = document.querySelectorAll('.font-preview');
-
-    // Loop through each preview and update the text
     previews.forEach(function(preview) {
-        // If there's user input, use it; otherwise, use the default text (from data-font-name)
         let defaultText = preview.getAttribute('data-font-name');
         preview.innerText = userInput || defaultText;
     });
