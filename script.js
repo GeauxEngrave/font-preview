c75df30b2443a988be51bb3494c1b323a966e556
GeauxEngrave <geauxengrave@gmail.com>
2025-02-09 11:59:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function updateText() {
     let userInput = document.getElementById('textInput').value;
-    document.getElementById('blackswordPreview').innerText = userInput || "Blacksword";
-    document.getElementById('openSansPreview').innerText = userInput || "Open Sans";
-    document.getElementById('serifPreview').innerText = userInput || "Times New Roman";
-    document.getElementById('monospacePreview').innerText = userInput || "Courier New";
-    document.getElementById('nathanPreview').innerText = userInput || "Nathan";
-    document.getElementById('timberlyScriptPreview').innerText = userInput || "TimberlyScript";
-    document.getElementById('ballistonePreview').innerText = userInput || "Ballistone";
-    document.getElementById('madelinaScriptPreview').innerText = userInput || "MadelinaScript";
-    document.getElementById('tastefulSandwichPreview').innerText = userInput || "TastefulSandwich";
-    document.getElementById('sweetyPreview').innerText = userInput || "Sweety";
-    document.getElementById('rochesterPreview').innerText = userInput || "Rochester";
-    document.getElementById('legoHousePreview').innerText = userInput || "LegoHouse";
     document.getElementById('abeeZeePreview').innerText = userInput || "ABeeZee";
+    document.getElementById('ballistonePreview').innerText = userInput || "Ballistone";
+    document.getElementById('blackswordPreview').innerText = userInput || "Blacksword";
+    document.getElementById('legoHousePreview').innerText = userInput || "LegoHouse";
+    document.getElementById('madelinaScriptPreview').innerText = userInput || "MadelinaScript";
+    document.getElementById('nathanPreview').innerText = userInput || "Nathan";
+    document.getElementById('rochesterPreview').innerText = userInput || "Rochester";
+    document.getElementById('sweetyPreview').innerText = userInput || "Sweety";
     document.getElementById('sweetyStrawberryPreview').innerText = userInput || "SweetyStrawberry";
+    document.getElementById('tastefulSandwichPreview').innerText = userInput || "TastefulSandwich";
+    document.getElementById('timberlyScriptPreview').innerText = userInput || "TimberlyScript";
     document.getElementById('theQueenPreview').innerText = userInput || "TheQueen";
 }
