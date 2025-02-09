5157b464c24cf7a48d40f8be1b03d4026a864185
GeauxEngrave <geauxengrave@gmail.com>
2025-02-09 11:40:21 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function updateText() {
+    let userInput = document.getElementById('textInput').value;
+    document.getElementById('blackswordPreview').innerText = userInput || "Blacksword";
+    document.getElementById('openSansPreview').innerText = userInput || "Open Sans";
+    document.getElementById('serifPreview').innerText = userInput || "Times New Roman";
+    document.getElementById('monospacePreview').innerText = userInput || "Courier New";
+    document.getElementById('nathanPreview').innerText = userInput || "Nathan";
+    document.getElementById('timberlyscriptPreview').innerText = userInput || "Timberly Script";
+    document.getElementById('ballistonePreview').innerText = userInput || "Ballistone";
+    document.getElementById('madelinascriptPreview').innerText = userInput || "Madelina Script";
+    document.getElementById('tastefulsandwichPreview').innerText = userInput || "Tasteful Sandwich";
+    document.getElementById('sweetyPreview').innerText = userInput || "Sweety";
+    document.getElementById('rochesterPreview').innerText = userInput || "Rochester";
+    document.getElementById('legohousePreview').innerText = userInput || "Lego House";
+    document.getElementById('abeezeePreview').innerText = userInput || "ABeeZee";
+    document.getElementById('sweetystrberryPreview').innerText = userInput || "Sweety Strawberry";
+    document.getElementById('thequeenPreview').innerText = userInput || "The Queen";
+    document.getElementById('centurygothicPreview').innerText = userInput || "Century Gothic";
+}
